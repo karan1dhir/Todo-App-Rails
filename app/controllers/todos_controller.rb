87a20fb0486871039873f5ec09b0977085ae25f9
karan1dhir <karandhir64@yahoo.co.in>
2018-06-23 18:29:21 +0530

    make it better

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -34,6 +34,12 @@ end
 def index
 	@todo=Todo.all
 end
+def destroy
+	@todo=Todo.find(params[:id])
+	@todo.destroy
+	flash[:notice]="Todo was successfully deleted"
+	redirect_to todos_path
+end	
  private 
   def todo_params
   params.require(:todo).permit(:name,:description)
